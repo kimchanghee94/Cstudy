9eccedbd723588ed6d4b99b021827d2f2418c7a4
kimchanghee <kimchanghee94@naver.com>
2025-09-19 09:51:22 +0900

    #main 1. 2908

--- src/2908.cpp
+++ src/2908.cpp
@@ -2,6 +2,20 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Test for");
-    return 0;
+    int A,B;
+    scanf("%d %d", &A, &B);
+    int tmpA=0, tmpB=0;
+    while(A>0){
+        tmpA+=A%10;
+        tmpA*=10;
+        A/=10;
+    }
+    
+    while(B>0){
+        tmpB+=B%10;
+        tmpB*=10;
+        B/=10;
+    }
+    tmpA/=10,tmpB/=10;
+    printf("%d\n", tmpA > tmpB ? tmpA : tmpB);
 }
