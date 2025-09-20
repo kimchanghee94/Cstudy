b3953aeb0aaa6deb56516ef4bb81aeb73a6755dd
kimchanghee <kimchanghee94@naver.com>
2025-09-20 23:20:19 +0900

    #main 1. c++문법보다 c문법이 메모리사용량 덜나옴

--- src/1244.cpp
+++ src/1244.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
 #define MAX 102
 int S,N,arr[MAX];
 
 int main(void){
-    ios::sync_with_stdio(0);cin.tie(0);
-    cin>>S;for(int i=1; i<=S; i++) cin>>arr[i];cin>>N;
+    scanf("%d", &S);for(int i=1; i<=S; i++) scanf("%d",&arr[i]);scanf("%d",&N);
     while(N-->0){
-        int sex,idx; cin>>sex>>idx;
+        int sex,idx; scanf("%d%d",&sex,&idx);
         if(sex==1) for(int i=idx; i<=S; i+=idx) arr[i]=!arr[i];
         else{
             if(idx==1 || idx==S) arr[idx]=!arr[idx];
@@ -26,7 +25,7 @@ int main(void){
         } 
     }
     for(int i=1; i<=S; i++) {
-        if(i%20) cout<<arr[i]<<" ";
-        else cout<<arr[i]<<"\n";
+        if(i%20) printf("%d ",arr[i]);
+        else printf("%d\n",arr[i]);
     }
 }
