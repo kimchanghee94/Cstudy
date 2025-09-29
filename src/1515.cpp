96f988d43d6377dc00166a2ed1e0e7bc8a1ec4e3
kimchanghee <kimchanghee94@naver.com>
2025-09-29 21:06:05 +0900

    #main 1. 1515

---
+++ src/1515.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);cin.tie(0);
+    string arr; cin >> arr;
+    int ans=1, idx=0;
+    while(1){
+        string ans_str=to_string(ans);
+        for(int i=0; i<ans_str.length(); i++){
+            if(ans_str[i]==arr[idx]) idx++;
+            if(idx==arr.length()){
+                cout << ans;
+                return 0;
+            }
+        }
+        ans++;
+    }
+}
