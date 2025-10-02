b66aa75db94a8c8dbaba5d2f661d12ab794df718
kimchanghee <kimchanghee94@naver.com>
2025-10-02 18:01:28 +0900

    #main 1. 1697

---
+++ src/1697.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#define MAX 100005
+int di[]={1,-1,0}, dj[]={1,1,2};
+
+int main(){
+    int N,K; cin>>N>>K;
+    int graph[MAX]={0};
+    graph[N]=1;
+    queue<int> q; q.push(N);
+    while(!q.empty()){
+        int fi=q.front();
+        if(fi==K) break;
+        q.pop();
+
+        for(int d=0; d<3; d++){
+            int ni=fi*dj[d]+di[d];
+            if(ni<0 || ni>=MAX || graph[ni]) continue;
+            graph[ni]=graph[fi]+1;
+            q.push(ni);
+        }
+    }
+    cout<<graph[K]-1;
+}
